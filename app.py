ccb27789d18c2475cc3ce91e02de913d29c87543
Erin McCoomer <168048280+erinmccoomer@users.noreply.github.com>
2026-09-01 13:37:13 -0400

    Update app.py
    Add initial developer profile

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 print("Name: Erin McCoomer")
 print("Major: Computer Science")
 print("Technology Interest: Data Analytics, Artificial Intelligence, and Web Design")
-print("Senior Project Skill Goal: Strengthening my project management and web design skills.")
+print("Senior Project Skills Goal: Strengthening my project management and web design skills.")
